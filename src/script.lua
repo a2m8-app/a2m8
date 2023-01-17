d24b3ef551b5f636c91f53c24b160780da4118a9
Tricked <tricked@tricked.pro>
2023-01-17 21:21:46 +0100

    more utils

--- src/script.lua
+++ src/script.lua
@@ -1,18 +1,7 @@
 ---@diagnostic disable: undefined-global
 
-local internal = require("event_handler_internal")
-local s = require('event_sender')
-s:type("Hello World")
-while (true) do
-    internal.event_handler.grab(function(event)
-        if event.key == "F8" then
-            os.exit()
-        end
-        if event.name == "key_press" and event.key ~= nil then
-            if match.random(0, 2) == 1 then
-                s:type("Hello World")
-            end
-        end
-        return event
-    end)
-end
+local sender = require("event_sender")
+
+sender:enter()
+sender:enter()
+sender.enter()
--- src/std/event_sender.lua
+++ src/std/event_sender.lua
@@ -4,7 +4,7 @@ local sim = events.simulate
 
 ---
 ---@param word string
-function events:type(word)
+function events.type(word)
     for i = 1, #word do
         local letter = string.sub(word, i, i);
         local name = "Key" .. letter:upper()
@@ -28,6 +28,26 @@ end
 ---@param time number time in seconds
 function events:move_to(x, y, time)
     -- use u.sleep(time) to sleep
+end
+
+function events.tab()
+    sim(events.create_key_press("Tab"))
+    sim(events.create_key_release("Tab"))
+end
+
+function events.enter()
+    sim(events.create_key_press("Return"))
+    sim(events.create_key_release("Return"))
+end
+
+---@param key string
+---@param delay number
+function events.press_key(key, delay)
+    sim(events.create_key_press(key))
+    if delay ~= nil then
+        u.sleep(delay)
+    end
+    sim(events.create_key_release(key))
 end
 
 return events
