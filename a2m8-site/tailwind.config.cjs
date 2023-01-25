1141a93be86f7980bafe337ebc8a98dd62d8c043
Tricked <tricked@tricked.pro>
2023-01-25 09:33:45 +0000

    tailwinding

---
+++ a2m8-site/tailwind.config.cjs
@@ -0,0 +1,16 @@
+const daisyui = require("daisyui");
+const basic = require("a2m8-shared/theme")
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+	daisyui: {
+		logs: false,
+		themes: [{ basic }],
+	},
+}
