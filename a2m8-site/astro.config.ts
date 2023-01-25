bdd812d217bf37cb98f941024828b48f959d48e0
Tricked <tricked@tricked.pro>
2023-01-25 12:31:03 +0000

    fx

--- a2m8-site/astro.config.ts
+++ a2m8-site/astro.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 import cloudflare from "@astrojs/cloudflare";
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(), 
+    mdx(),		
+    preact(),
+    react(),
+  ],
+	site: `https://astro.build`,
   output: "server",
   adapter: cloudflare()
 });
